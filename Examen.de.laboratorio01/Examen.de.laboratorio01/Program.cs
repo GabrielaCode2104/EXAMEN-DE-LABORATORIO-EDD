a2a8fa472dfd37e641f802bb4721dda83990b41f
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-16 10:37:02 -0500

    exam

--- Examen.de.laboratorio01/Examen.de.laboratorio01/Program.cs
+++ Examen.de.laboratorio01/Examen.de.laboratorio01/Program.cs
@@ -2,44 +2,44 @@
 
 //#1 PEDIR LADOS DE UN RECTANGULO Y DIBUJAR EN PANTALLA
 
-int lado, ancho;
-while (true)
-{
-    Console.Write("Ingrese el LADO del rectangulo: ");
-    if (int.TryParse(Console.ReadLine(), out lado) && lado > 0)
-    {
-        break;
-    }
+//int lado, ancho;
+//while (true)
+//{
+//    Console.Write("Ingrese el LADO del rectangulo: ");
+//    if (int.TryParse(Console.ReadLine(), out lado) && lado > 0)
+//    {
+//        break;
+//    }
 
-    else
-    {
-        Console.WriteLine("DATO INVALIDO. VUELVA A INGRESAR DATO.");
+//    else
+//    {
+//        Console.WriteLine("DATO INVALIDO. VUELVA A INGRESAR DATO.");
 
-    }
-}
+//    }
+//}
 
-while (true)
-{
-    Console.WriteLine("Ingrese la ANCHO del rectangulo: ");
-    if (int.TryParse(Console.ReadLine(), out ancho) && ancho > 0)
-    {
-        break;
-    }
+//while (true)
+//{
+//    Console.WriteLine("Ingrese la ANCHO del rectangulo: ");
+//    if (int.TryParse(Console.ReadLine(), out ancho) && ancho > 0)
+//    {
+//        break;
+//    }
 
-    else
-    {
-        Console.WriteLine("DATO INVALIDO. VUELVA A INGRESAR DATO.");
-    }
-}
+//    else
+//    {
+//        Console.WriteLine("DATO INVALIDO. VUELVA A INGRESAR DATO.");
+//    }
+//}
 
-for (int i = 0; i < lado; i++)
-{
-    for (int j = 0; j < ancho; j++)
-    {
-        Console.Write("*");
-    }
-    Console.WriteLine();
-}
+//for (int i = 0; i < lado; i++)
+//{
+//    for (int j = 0; j < ancho; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
 
 
 
@@ -47,31 +47,31 @@ for (int i = 0; i < lado; i++)
 
 
 
-//Console.WriteLine("INGRESE EL TAMAÑO DEL HEXÁGONO: ");
-//int sizeTam = Convert.ToInt32(Console.ReadLine());  
+Console.WriteLine("INGRESE EL TAMAÑO DEL HEXÁGONO: ");
+int sizeTam = Convert.ToInt32(Console.ReadLine());
 
-//for (int i = 0; i < sizeTam; i++)
-//{
-//    for (int j = 0; j < sizeTam - i; j++)
-//    {
-//        Console.Write(" ");
-//    }
-//    for (int k = 0; k < 2 * i + 1; k++)
-//    {
-//        Console.Write("H");
-//    }
-//    Console.WriteLine();
-//}
+for (int i = 0; i < sizeTam; i++)
+{
+    for (int j = 0; j < sizeTam - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < 2 * i + 1; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
 
-//for (int i =  sizeTam - 2; i>= 0; i--)
-//{
-//    for(int j = 0;j < sizeTam - i; j++)
-//    {
-//        Console.Write(" ");
-//    }
-//    for (int k = 0; k < 2*i+1; k++)
-//    {
-//        Console.Write("H");
-//    }
-//    Console.WriteLine();
-//}
+for (int i = sizeTam - 2; i >= 0; i--)
+{
+    for (int j = 0; j < sizeTam - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < 2 * i + 1; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
